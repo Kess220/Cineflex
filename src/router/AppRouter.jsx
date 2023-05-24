8415433a8c24fd8436b054c41f51eb819d043e35
unknown <prodvictorkaio@gmail.com>
2023-05-24 20:38:16 -0300

    resolvido error nav

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SessionsPage from "../pages/SessionsPage/SessionsPage";
-import HomePage from "../pages/HomePage/HomePage";
+import App from "../App";
 
-function App() {
+function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<App />} />
         <Route path="/sessoes/:movieId" element={<SessionsPage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default AppRouter;
